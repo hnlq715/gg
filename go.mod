87f9a4d047fabf91923ecf2924a8ca1916e15eb7
大可 <hnlq.sysu@gmail.com>
2023-06-27 16:06:45 +0800

    feat: add gg tool

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/hnlq715/gg
+
+go 1.20
+
+require (
+	github.com/samber/lo v1.38.1
+	github.com/urfave/cli v1.22.14
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+
+	"github.com/samber/lo"
+	"github.com/urfave/cli"
+)
+
+var workspace = flag.String("workspace", filepath.Join(lo.Must(os.UserHomeDir()), "workspace"), "workspace path")
+
+func main() {
+
+	app := cli.NewApp()
+	app.Usage = "Simple git repo clone tool with workspace support"
+	app.Action = func(c *cli.Context) error {
+		gitpath := c.Args().First()
+		parsedPath := lo.Must(url.Parse(gitpath))
+
+		clonePath := filepath.Join(
+			lo.Must(filepath.Abs(*workspace)), parsedPath.Host, parsedPath.Path)
+
+		_, err := os.Stat(clonePath)
+		if os.IsNotExist(err) {
+			fmt.Println("git", "clone", gitpath, clonePath)
+			lo.Must0(exec.Command("git", "clone", gitpath, clonePath).Run())
+		} else {
+			fmt.Println(clonePath, "already exists")
+		}
+
+		return nil
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
